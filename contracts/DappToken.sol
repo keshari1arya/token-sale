8411903e3d5eb2c1fa3ffdd820db03126d2ec945
archoudhury <archoudhury@teksystems.com>
2021-07-16 11:27:53 +0530

    Transfer token

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,14 +1,27 @@
-pragma solidity ^0.5.16;
+pragma solidity >=0.5.16 <0.9.0;
 
 contract DappToken {
     uint256 public totalSupply;
     string public name = "DApp Token";
     string public symbol = "DAPP";
 
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
     mapping(address => uint256) public balanceOf;
 
     constructor(uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
+    }
+
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool sussess)
+    {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
     }
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -7,11 +7,12 @@ require('chai')
     .should()
 
 contract('DappToken', function (accounts) {
-let dappToken;
+    let dappToken;
     before(async () => {
         dappToken = await DappToken.deployed();
     })
-    it('Initializes contract with correct values', async()=>{
+
+    it('Initializes contract with correct values', async () => {
         assert.equal(await dappToken.name(), "DApp Token");
         assert.equal(await dappToken.symbol(), "DAPP");
     });
@@ -23,5 +24,34 @@ let dappToken;
 
         assert.equal(adminBalance.toNumber(), 1000000);
         assert.equal(totalSupply.toNumber(), 1000000);
+    });
+
+    it('should fail when low balance', async () => {
+        try {
+            await dappToken.transfer(accounts[1], 1000001);
+            assert.fail("The transaction should have thrown an error");
+        }
+        catch (err) {
+            assert.include(err.message, "revert", "The error message should contain 'revert'");
+        }
+    });
+
+    it('should transfer token', async () => {
+        var acc0Balance = +await dappToken.balanceOf(accounts[0]);
+        var acc1Balance = +await dappToken.balanceOf(accounts[1]);
+        console.log(acc1Balance)
+
+        var receipt = await dappToken.transfer(accounts[1], 10, { from: accounts[0] });
+
+        assert.lengthOf(receipt.logs, 1, "Invalid event logs");
+        assert.equal(receipt.logs[0].event, 'Transfer');
+        assert.equal(receipt.logs[0].args._to, accounts[1]);
+        assert.equal(receipt.logs[0].args._from, accounts[0]);
+        assert.equal(receipt.logs[0].args._value, 10);
+        assert.equal(await dappToken.balanceOf(accounts[0]), acc0Balance - 10, "Error in sender balance");
+        assert.equal(await dappToken.balanceOf(accounts[1]), acc1Balance + 10, "Error in receiver balance");
+
+        var value = await dappToken.transfer.call(accounts[1], 10, { from: accounts[0] });
+        assert.isTrue(value, "function should return true if all execution success");
     });
 });
