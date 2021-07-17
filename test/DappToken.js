7f07c601dcdf5b5de4ba94f69a4b965c532680ee
archoudhury <archoudhury@teksystems.com>
2021-07-17 12:13:22 +0530

    deligate transfer

--- test/DappToken.js
+++ test/DappToken.js
@@ -54,4 +54,60 @@ contract('DappToken', function (accounts) {
         var value = await dappToken.transfer.call(accounts[1], 10, { from: accounts[0] });
         assert.isTrue(value, "function should return true if all execution success");
     });
+
+    it('should approve tokens for delegated transfer', async () => {
+        var success = await dappToken.approve.call(accounts[1], 100);
+        assert.isTrue(success, "approve returns true");
+
+        var receipt = await dappToken.approve(accounts[1], 100, { from: accounts[0] });
+        assert.lengthOf(receipt.logs, 1, "Invalid event logs");
+        assert.equal(receipt.logs[0].event, 'Approval');
+        assert.equal(receipt.logs[0].args._owner, accounts[0]);
+        assert.equal(receipt.logs[0].args._spender, accounts[1]);
+        assert.equal(receipt.logs[0].args._value, 100);
+
+        var allowance = await dappToken.allowance(accounts[0], accounts[1]);
+        assert.equal(allowance.toNumber(), 100, 'Allowance should be 100');
+    });
+
+    it('should handle delegate transfer', async () => {
+        var fromAccount = accounts[2];
+        var toAccount = accounts[3];
+        var spendingAccount = accounts[4];
+        var transferReceipt = await dappToken.transfer(fromAccount, 100, { from: accounts[0] });
+        var approveReceipt = await dappToken.approve(spendingAccount, 10, { from: fromAccount });
+
+        try {
+            var transferFromReceipt = await dappToken.transferFrom(fromAccount, toAccount, 9999, { from: spendingAccount });
+            assert.fail("The transaction should have thrown an error");
+        } catch (error) {
+            assert.include(error.message, "revert", "Cant transfer amount more than balance");
+        }
+
+        try {
+            var transferFromReceipt = await dappToken.transferFrom(fromAccount, toAccount, 20, { from: spendingAccount });
+            assert.fail("The transaction should have thrown an error");
+        } catch (error) {
+            assert.include(error.message, "revert", "Cant transfer amount more than allowance");
+        }
+
+        var success = await dappToken.transferFrom.call(fromAccount, toAccount, 10, { from: spendingAccount });
+        assert.isTrue(success);
+
+        var receipt = await dappToken.transferFrom(fromAccount, toAccount, 10, { from: spendingAccount });
+        assert.lengthOf(receipt.logs, 1, "Invalid event logs");
+        assert.equal(receipt.logs[0].event, 'Transfer');
+        assert.equal(receipt.logs[0].args._from, fromAccount);
+        assert.equal(receipt.logs[0].args._to, toAccount);
+        assert.equal(receipt.logs[0].args._value, 10);
+
+        var balanceOfFromAccount = await dappToken.balanceOf(fromAccount);
+        assert.equal(balanceOfFromAccount.toNumber(), 90);
+
+        var balanceOfToAccount = await dappToken.balanceOf(toAccount);
+        assert.equal(balanceOfToAccount.toNumber(), 10);
+
+        var allowance = await dappToken.allowance(fromAccount, spendingAccount);
+        assert.equal(allowance.toNumber(), 0);
+    });
 });
